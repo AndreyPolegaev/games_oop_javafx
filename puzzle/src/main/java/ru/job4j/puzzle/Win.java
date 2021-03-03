87c732062ea6a9a62427852039e05d56f5a221c6
AndreyPolegaev <79646307753@yandex.ru>
2021-03-03 15:17:43 +0300

    2. Сокобан. Тестовое задание. [#466184]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -1,8 +1,55 @@
 package ru.job4j.puzzle;
-
+/*
+добавить еще 2 дополнительных метода, которые будут
+проверять строку и столбец на нашем игровом поле, которые мы будем вызывать в методе check() при определенных условиях.
+ */
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = true;
+        return (checkVertical(board) || checkHorizontal(board));
+    }
+    public static boolean checkVertical(int[][] board) {
+        boolean rsl = false;
+        boolean[] step = new boolean[board.length];
+        for (int i = 0; i < board.length; i++) {
+            boolean condition = false;
+            for (int j = 0; j < board.length; j++) {
+                if (board[j][i] != 1) {
+                    condition = true;
+                    break;
+                }
+            }
+            if (!condition) {
+                step[i] = true;
+            }
+        }
+        for (boolean temp : step) {
+            if (temp == true) {
+                rsl = true;
+            }
+        }
+        return rsl;
+    }
+
+    public static boolean checkHorizontal(int[][] board) {
+        boolean rsl = false;
+        boolean[] step = new boolean[board.length];
+        for (int i = 0; i < board.length; i++) {
+            boolean condition = false;
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] != 1) {
+                    condition = true;
+                    break;
+                }
+            }
+            if (!condition) {
+                step[i] = true;
+            }
+        }
+        for (boolean temp : step) {
+            if (temp == true) {
+                rsl = true;
+            }
+        }
         return rsl;
     }
 }
