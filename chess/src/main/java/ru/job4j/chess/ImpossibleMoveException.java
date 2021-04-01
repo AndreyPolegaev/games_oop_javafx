74568caa7b57da07ce9c986f2a438be1c0c081ce
AndreyPolegaev <79646307753@yandex.ru>
2021-04-01 23:41:17 +0300

    2. Каркас шахматной доски [#466335]

--- chess/src/main/java/ru/job4j/chess/ImpossibleMoveException.java
+++ chess/src/main/java/ru/job4j/chess/ImpossibleMoveException.java
@@ -1,7 +1,9 @@
 package ru.job4j.chess;
 
+import ru.job4j.chess.firuges.Cell;
+
 public class ImpossibleMoveException extends RuntimeException {
-    public ImpossibleMoveException(String message) {
+    public ImpossibleMoveException(String message, Cell position, Cell dest) {
         super(message);
     }
 }
